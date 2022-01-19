6e1225f21c29baa642f41007e2af532131d53041
Ibrahim Albrahim <hemo7f12@gmail.com>
2022-01-20 01:58:24 +0300

    using for loop instead of repeating element manually and fix style

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../contact';
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  contacts: Contact[] = [
+    {'name': 'telegram',       'link': 'https://t.me/hemo7f12'},
+    {'name': 'whatsapp',       'link': 'https://wa.me/966565091858'},
+    {'name': 'twitter',        'link': 'https://twitter.com/hemo7f12'},
+    {'name': 'envelope',       'link': 'mailto: hemo7f12@gmail.com'},
+    {'name': 'snapchat-ghost', 'link': 'https://www.snapchat.com/add/hemo7ss'},
+    {'name': 'instagram',      'link': 'https://www.instagram.com/hemo7f12/'},
+    {'name': 'linkedin',       'link': 'https://www.linkedin.com/in/ibrahim-albrahim-hemo7f12/'},
+    {'name': 'github',         'link': 'https://github.com/ibrahim-albrahim/'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
